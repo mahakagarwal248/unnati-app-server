12b71f18e51c06d27ba1227ad4e8be3a8a23600c
Mahak Agarwal <mahak.agarwal@techchefz.com>
2023-06-01 08:37:07 +0530

    minor fix

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("This is a shopping site API");
+  res.send("This is Unnati server API");
 });
 
 app.use("/user", userRoutes);
